1c2dfef7455636126e8edcda451c59d5162ff4f3
Karl Stolley <karl.stolley@gmail.com>
2011-07-01 12:43:32 -0500

    CSS and JS to own files; improved CSS for JavaScriptless use

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # NWC
 
 Binder/branding for Cheryl B. & Ryan T. for the New Work of Composing book for CCDP.
+
+## Needed Improvements for the Future
+  * Add a hover/active state to ``div#nwcbinder h1 a``, primarily to ensure keyboard access
---
+++ binder/binder.js
@@ -0,0 +1,19 @@
+/********************
+*
+* New Work of Composing, Journet, Ball, & Trauman, eds.
+* binder.js
+* 
+* v. 0.1
+* 07/01/2011
+*
+* see http://github.com/karlstolley/nwc
+*
+*********************/
+
+$(document).ready(function() {
+  $('html').addClass('hasjs'); /*Put hasjs on html*/
+  $('#nwctitle a').click(function() { /*Listen for clicks/keyboard activation on #nwctitle a*/
+    $('html').toggleClass('nwcvisible'); /*Toggle the nwcvisible class on alternating clicks*/
+    return false; /*Prevent the default behavior*/
+  });
+});
